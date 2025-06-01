f2027ca1e0d850957a57450bed8d8b38d31e4ff3
AmartyaDev007 <162921312+AmartyaDev007@users.noreply.github.com>
2025-06-01 12:26:16 +0530

    Add js functionality to the change grid size button

--- code.js
+++ code.js
@@ -2,10 +2,8 @@ let gridSize = 16;
 
 const mainContainer = document.querySelector(".container"); 
 
-
-function createGrid () {
+function createGrid() {
     for (let i = 0; i < gridSize; i++) {
-
         const loopDivRow = document.createElement("div")
         loopDivRow.classList.add("loop-div-row");
         mainContainer.appendChild(loopDivRow);
@@ -18,20 +16,32 @@ function createGrid () {
     }
 }
 
-
-
 createGrid();
 
-
 mainContainer.addEventListener('mousemove', (event) => {
-
     const target = event.target;
 
     if (target.classList.contains("loop-div-row") || (target.classList.contains("container"))) {
         return
     }
-
     else {
         target.style.backgroundColor = "lightblue"
     }
 });
+
+
+const changeGridSizeBtn = document.querySelector("#change-size-grid");
+const resetBtn = document.querySelector("#reset");
+const randomColorBtn = document.querySelector("#random-colors");
+
+
+changeGridSizeBtn.addEventListener('click', () => {
+
+    gridSize = prompt("What grid size? (max. 100)");
+
+    mainContainer.innerHTML = '';
+
+    createGrid();
+});
+
+
--- todo.md
+++ todo.md
@@ -1,14 +1,17 @@
 - Create a default grid (16x16)
 - add a button that prompts the user for custom grid size (max 100)
 - 960px wide ??!?
-- set up click and drag effect that will change color
+- ~~set up click and drag effect that will change color~~ just using drag for now
 - Random color choice
 - reset button 
+- add opacity to grid
 
-
-#### Optional
+#### In the future
 
 - Custom color choices 
 
 - Eraser to remove grid color
 
+- click and drag instead of just drag
+
+- maybe add current grid size 
