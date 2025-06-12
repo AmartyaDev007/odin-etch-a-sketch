6399bf5ae2f37fdc869da9d34ae80aef3352b4df
AmartyaDev007 <162921312+AmartyaDev007@users.noreply.github.com>
2025-06-12 13:45:21 +0530

    Finalize the project

--- code.js
+++ code.js
@@ -18,30 +18,61 @@ function createGrid() {
 
 createGrid();
 
-mainContainer.addEventListener('mousemove', (event) => {
+function randomRGBValue() {
+    return Math.floor(Math.random() * 255);
+}
+
+
+function randomColor() {
+    return `rgb(${randomRGBValue()}, ${randomRGBValue()}, ${randomRGBValue()})`;
+}
+
+
+function opacitySet(theTarget) {
+    let currentOpacity = Number(theTarget.style.opacity) || 0;
+    
+    if (currentOpacity >= 1) {
+        return 1;
+    }
+
+    return currentOpacity + 0.1;
+}
+
+
+mainContainer.addEventListener('mouseenter', (event) => {
     const target = event.target;
 
     if (target.classList.contains("loop-div-row") || (target.classList.contains("container"))) {
         return
     }
     else {
-        target.style.backgroundColor = "lightblue"
+        target.style.backgroundColor = randomColor();
+        target.style.opacity = opacitySet(target);
     }
-});
+}, true);
 
 
 const changeGridSizeBtn = document.querySelector("#change-size-grid");
 const resetBtn = document.querySelector("#reset");
-const randomColorBtn = document.querySelector("#random-colors");
-
 
 changeGridSizeBtn.addEventListener('click', () => {
-
     gridSize = prompt("What grid size? (max. 100)");
 
-    mainContainer.innerHTML = '';
+    if (gridSize > 100) {
+        alert("MAX ALLOWED SIZE OF GRID IS 100!")
+        return;
+    }
 
+    mainContainer.innerHTML = '';
     createGrid();
 });
 
+
+resetBtn.addEventListener('click', () => {
+    const allDivs = mainContainer.querySelectorAll('.loop-div');
+
+    allDivs.forEach(div => {
+        div.removeAttribute('style');
+      });
+});
 
